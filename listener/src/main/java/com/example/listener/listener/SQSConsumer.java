35219d4641cbcd0dd873e56a548d9dff7eed2760
Abiduddin <abidakash456@gmail.com>
2022-04-14 23:42:47 +0600

    project init

---
+++ listener/src/main/java/com/example/listener/listener/SQSConsumer.java
@@ -0,0 +1,17 @@
+package com.example.listener.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class SQSConsumer {
+
+    @SqsListener("${cloud.aws.queue.name}")
+    public void receiveMessage(Map<String, Object> message) {
+        log.info("SQS Message Received : {}", message);
+    }
+}
